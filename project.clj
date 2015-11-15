4f04d16644b2a3d951379c5740a339192f6dbe76
Matthew King <mking@users.noreply.github.com>
2015-11-15 06:47:48 -0800

    update async

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/core.async "0.2.371"]
+                 [org.clojure/core.async "0.2.374"]
                  [cljsjs/react "0.14.0-1"]
                  [cljsjs/react-dom "0.14.0-1"]
                  [sablono "0.4.0"]
@@ -19,7 +19,7 @@
                  [secretary "1.2.3"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0"]]
+            [lein-figwheel "0.5.0-1"]]
 
   :source-paths ["src"]
 

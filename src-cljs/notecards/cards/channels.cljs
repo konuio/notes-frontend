4b73d5f7c3a1656a48b9148c623b772d25bd74ae
Matthew King <mking@users.noreply.github.com>
2015-11-15 19:37:33 -0800

    add pubsub example

--- src-cljs/notecards/cards/channels.cljs
+++ src-cljs/notecards/cards/channels.cljs
@@ -4,7 +4,7 @@
   (:require [om.core :as om]
             [sablono.core :refer-macros [html]]
             [shodan.console :as console]
-            [cljs.core.async :as async :refer [chan put! <! >! timeout alts! buffer dropping-buffer sliding-buffer]]))
+            [cljs.core.async :as async :refer [pub sub chan put! <! >! timeout alts! buffer dropping-buffer sliding-buffer]]))
 
 ; Unused from cljs
 ; <!! (blocking take)
@@ -19,7 +19,7 @@
 ; Buffer kinds
 ; - For nonblocking buffers, we can always immediately put a message into the queue (the message might evict existing messages).
 ; - For blocking buffers, we are stuck if the queue is full. I feel like, if we use put! onto a blocking buffer, we end up with an infinite queue (the queued puts).
-(console/log "blocking?:" {:sliding (async/unblocking-buffer? (sliding-buffer 3))
+#_(console/log "blocking?:" {:sliding (async/unblocking-buffer? (sliding-buffer 3))
                            :dropping (async/unblocking-buffer? (dropping-buffer 3))
                            :buffer (async/unblocking-buffer? (buffer 3))})
 
@@ -80,3 +80,66 @@
 
 #_(defcard buffer-card
          (om-root buffer-view))
+
+; This is a pretty good example of pubsub.
+; - Pubsub is a "marker" extension of channels.
+; - The fundamental channel interface remains unchanged. What changes is how messages are propagated after they enter core.async land.
+; Some rules for pub
+; - You cannot listen to the raw channel once it is wrapped in a pub (this will multiplex with the pub).
+; - pub is a way to SPLIT a channel into multiple channels.
+; - sub is a way to LISTEN to a splitted part from the pub channel.
+; - ALL subs hear the message that is received by a pub.
+; - It is possible to JOIN multiple topic streams onto a single channel using sub. I feel like pub and sub are mostly useful in an imperative manner.
+(defn pub-view [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      (let [ch (chan)
+            pub (pub ch #(:topic %))]
+        {:ch ch
+         :pub pub}))
+    om/IWillMount
+    (will-mount [_]
+      (let [{:keys [pub]} (om/get-state owner)
+            red-ch (chan)
+            red-backup-ch (chan)
+            blue-ch (chan)
+            color-ch (chan)
+            red-sub (sub pub :red red-ch)
+            red-backup-sub (sub pub :red red-backup-ch)
+            blue-sub (sub pub :blue blue-ch)
+            color-red-sub (sub pub :red color-ch)
+            color-blue-sub (sub pub :blue color-ch)]
+        (go (loop []
+              (let [message (<! red-ch)]
+                (console/log "red message:" message)
+                (recur))))
+        (go (loop []
+              (let [message (<! red-backup-ch)]
+                (console/log "red backup message:" message)
+                (recur))))
+        (go (loop []
+              (let [message (<! blue-ch)]
+                (console/log "blue message:" message)
+                (recur))))
+        (go (loop []
+              (let [message (<! color-ch)]
+                (console/log "color message:" message)
+                (recur))))))
+    om/IRenderState
+    (render-state [_ state]
+      (let [{:keys [ch]} state]
+        (html [:div
+               [:div
+                [:button.ChannelCards-button.ChannelCards-button--red
+                 {:type "button"
+                  :on-click (fn [e]
+                              (put! ch {:topic :red}))}]]
+               [:div
+                [:button.ChannelCards-button.ChannelCards-button--blue
+                 {:type "button"
+                  :on-click (fn [e]
+                              (put! ch {:topic :blue}))}]]])))))
+
+#_(defcard pub-card
+         (om-root pub-view))
